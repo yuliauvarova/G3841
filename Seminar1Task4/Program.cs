6ce35e8acebbf431b2216393fb28f7a57db73959
JuliaUvarova <yuliasennikova@yandex.ru>
2023-01-16 18:12:55 +0300

    Add solution Task 4

---
+++ Seminar1Task4/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из
+// этих чисел
+
+//Считываем данные с консоли 
+string? numberA = Console.ReadLine();
+string? numberB = Console.ReadLine();
+string? numberC = Console.ReadLine();
+
+if (numberA!=null && numberB!=null && numberC!=null)
+{
+    //Парсим введенные числа
+    int NumA = int.Parse(numberA);
+    int NumB = int.Parse(numberB);
+    int NumC = int.Parse(numberC);
+
+//Сравниваем числа
+        if(NumA>NumB)
+        
+        {
+            if(NumA>NumC)
+            Console.WriteLine("Первое число больше второго и третьего");
+        }
+        else
+        if (NumB>NumC)
+        {
+            Console.WriteLine("Второе число больше первого и третьего");
+        }
+        else 
+        Console.WriteLine("Третье число больше первого и второго");
+}
+          
