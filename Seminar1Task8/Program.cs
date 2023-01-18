7047885a63ccfa94e326a2b5cbb9f31815e60306
JuliaUvarova <yuliasennikova@yandex.ru>
2023-01-18 12:44:26 +0300

    Add solution Task 8

---
+++ Seminar1Task8/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает
+// все чётные числа от 1 до N.
+
+//Считываем данные с консоли 
+string? N = Console.ReadLine();
+
+//Делаем проверку на пустое значение
+if (N!=null)
+{
+    //Парсим введенное число
+    int outputNumber = int.Parse(N);
+
+    string R = string.Empty;
+
+    for(int i = 2; i < outputNumber; i = i+2)
+    {
+        R=R + i + ",";
+    }
+
+    if(outputNumber%2==0)
+    { 
+        R=R+outputNumber;
+    }
+
+        Console.WriteLine(R); 
+}
+    
