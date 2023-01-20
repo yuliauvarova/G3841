2942c3f5c4e68395c82e7c22b73b4c67b7c31bab
JuliaUvarova <yuliasennikova@yandex.ru>
2023-01-20 14:44:49 +0300

    add solutions tasks 10,13,13a,15

---
+++ Seminar2Task10/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на входе трехзначное число,
+//а на выходе показывает вторую цифру этого числа.
+
+int num = int.Parse(Console.ReadLine()??"0");
+
+if(num<1000 && num>99)
+{
+    int result = num/10;
+    result = result%10;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Число не трехзначное");
+}
---
+++ Seminar2Task13/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или 
+//сообщает, что третьей цифры нет
+
+Char[] digit = Console.ReadLine().ToCharArray();
+
+if(digit.Length > 2)
+{
+    Console.WriteLine(digit[2]);
+}
+else
+{
+Console.WriteLine("Третьй цифры нет");
+}
