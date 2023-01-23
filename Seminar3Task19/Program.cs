04abaace5741aeaa5fd84fb21fb9eb4edb993250
JuliaUvarova <yuliasennikova@yandex.ru>
2023-01-23 14:59:22 +0300

    add solution tasks 19,21,23

---
+++ Seminar3Task19/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом.
+
+//Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Выводит результат пользователю
+void PrintData(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+//Метод, определяет, палиндром ли это
+bool PalinTest(int n)
+{
+    bool result = false;
+    int d1 = n/10000;
+    int d2 = (n/1000)%10;
+    int d3 = (n/10)%10;
+    int d4 = n%10;
+    result = ((d1==d4) && (d2==d3))?true:false;
+    return result;
+}
+
+//Вводим число
+int n = ReadData("Введите пятизначное число: ");
+
+//Применяем метод к n
+bool res = PalinTest(n);
+
+//Выводим результат
+if(res)
+{ 
+    PrintData("Палиндром");
+}
+else
+{
+    PrintData("Не палиндром");
+}
---
+++ Seminar3Task21/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит
+// расстояние между ними в 3D пространстве.
+
+//Вводим координаты точек
+int x1 = ReadData("Введите координату X первой точки");
+int y1 = ReadData("Введите координату Y первой точки");
+int z1 = ReadData("Введите координату Z первой точки");
+int x2 = ReadData("Введите координату X второй точки");
+int y2 = ReadData("Введите координату Y второй точки");
+int z2 = ReadData("Введите координату Z второй точки");
+
+//Вычисляем длину
+double len = CalcLen(x1,x2,y1,y2,z1,z2);
+
+//Даем результат
+PrintData("Расстояние между точками: ",len);
+
+//Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+
+//Выводим результат пользователю
+void PrintData(string msg, double res)
+{
+    Console.WriteLine(msg + res);
+}
+
+//Вычисляем расстояние между точками в 3D пространстве
+double CalcLen(int x1,int x2,int y1,int y2, int z1, int z2)
+{
+    double result =0;
+    result = Math.Sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2));
+    return result;
+}
