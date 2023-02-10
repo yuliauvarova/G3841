057a275a7b20c0de36f81b482833acfc140a1147
JuliaUvarova <yuliasennikova@yandex.ru>
2023-02-10 12:20:56 +0300

    add solution tasks 54,56

---
+++ Seminar8Task54/Program.cs
@@ -0,0 +1,82 @@
+﻿// Задайте двумерный массив. Напишите программу, которая 
+// упорядочит по убыванию элементы каждой строки двумерного массива.
+
+//Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Метод генерирует двумерный массив
+int[,] Gen2DArray(int rows, int columns)
+{
+    int[,] arr = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            arr[i, j] = new Random().Next(1, 1000);
+        }
+    }
+    return arr;
+}
+
+//Метод выводит двумерный массив на экран
+void Print2DArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+//Метод сортировки одномерного массива пузырьком
+void BubbleSort(int[] arr)
+{
+    int j=0;
+    for(int k=0; k<arr.Length; k++)
+    {
+            for(int i=0; i<arr.Length-1; i++)
+        {
+            j=i+1;
+            if(arr[i]>arr[j])
+            {
+                int temporary =arr[i];
+                arr[i]=arr[j];
+                arr[j]=temporary;
+            }
+        }
+    }
+}
+
+//Метод упорядочивает по убыванию все элементы каждой строки
+void OrderLineElements(int[,] arr)
+{
+    for(int p=0; p<arr.GetLength(0); p++)
+    {
+        int [] newArrLine = new int[arr.GetLength(0)];
+        
+        for(int l=0; l<arr.GetLength(1); l++)
+        {
+            newArrLine[l]= arr[p,l];
+        }
+        BubbleSort(newArrLine);
+         for(int l=0; l<arr.GetLength(1); l++)
+        {
+            arr[p,arr.GetLength(1)-1-l]= newArrLine[l];
+        }
+    }
+}
+
+int m= ReadData("Введите число строк: ");
+int n= ReadData("Введите число столбцов: ");
+int[,] arr =Gen2DArray(m,n);
+Print2DArray(arr);
+Console.WriteLine();
+OrderLineElements(arr);
+Print2DArray(arr);
---
+++ Seminar8Task56/Program.cs
@@ -0,0 +1,87 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, 
+// которая будет находить строку с наименьшей суммой элементов.
+
+//Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Метод генерирует двумерный массив
+int[,] Gen2DArray(int rows, int columns)
+{
+    int[,] arr = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return arr;
+}
+
+//Метод выводит двумерный массив на экран
+void Print2DArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+//Метод ищет строку с наименьшей суммой элементов
+int SearchMinSumLine(int[,] arr)
+{
+
+    int minLine = 0;
+    int min = int.MaxValue;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            sum = sum + arr[i, j];
+        }
+        if (sum < min)
+        {
+            min = sum;
+            minLine = i;
+        }
+    }
+    return minLine;
+}
+
+//Метод выводит на экран одну строку массива
+void PrintLine(int[,] arr, int Line)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        Console.Write(arr[Line, j] + "\t");
+    }
+}
+
+
+
+int rows = ReadData("Введите число строк: ");
+int columns = ReadData("Введите число столбцов: ");
+
+if (rows != columns)
+{
+    int[,] arr = Gen2DArray(rows, columns);
+    Print2DArray(arr);
+    Console.WriteLine();
+    int minLine = SearchMinSumLine(arr);
+    PrintLine(arr,minLine);
+
+}
+else
+{
+    Console.WriteLine("С этими значениями невозможно построить прямоугольный массив.");
+}
+
