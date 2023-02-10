f5be5d92bb407886ccc7b4caecb40a41ad106be9
JuliaUvarova <yuliasennikova@yandex.ru>
2023-02-10 12:23:32 +0300

    add solution task 65

---
+++ Seminar9Task65/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте значения M и N. Напишите программу, которая 
+// найдёт все натуральные элементы в промежутке от M до N.
+
+//Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+string ReclineMN(int m, int n)
+{
+    if(m>=n)
+    {return n.ToString();}
+    else
+    {return m + " " + ReclineMN(m+1,n);}
+}
+
+//Выводит результат пользователю
+void PrintData(string msg)
+{
+    Console.WriteLine(msg);
+
+}
+
+int n=ReadData("Введите N: ");
+int m=ReadData("Введите M: ");
+string result=ReclineMN(m,n);
+PrintData(result);
