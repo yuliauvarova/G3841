04b46847f5d569fd6af8c37344aaf35441deec77
JuliaUvarova <yuliasennikova@yandex.ru>
2023-02-15 11:25:02 +0300

    add solutions tasks 58,60

---
+++ Seminar8Task58/Program.cs
@@ -0,0 +1,74 @@
+﻿//Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+//Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Метод генерирует двумерный массив
+int[,] Gen2DArray(int rows, int columns)
+{
+    int[,] arr = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            arr[i, j] = new Random().Next(1, 100);
+        }
+    }
+    return arr;
+}
+
+//Метод выводит двумерный массив на экран
+void Print2DArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+//Метод перемножает две матрицы
+int[,] MatrixMultiplication(int[,] matrixA, int[,] matrixB)
+    {       
+ if (matrixA.GetLength(1) != matrixB.GetLength(0))
+        {
+            Console.WriteLine("Умножение не возможно! Количество столбцов первой матрицы не равно количеству строк второй матрицы.");
+        }
+
+        int[,] matrixC = new int[matrixA.GetLength(0), matrixB.GetLength(1)];
+
+        for (int i = 0; i < matrixA.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrixB.GetLength(1); j++)
+            {
+                matrixC[i, j] = 0;
+
+                for (int k = 0; k < matrixA.GetLength(1); k++)
+                {
+                    matrixC[i, j] += matrixA[i, k] * matrixB[k, j];
+                }
+            }
+        }
+
+        return matrixC;
+    }
+
+
+
+int rows = ReadData("Введите число строк: ");
+int columns = ReadData("Введите число столбцов: ");
+int[,] arr1 = Gen2DArray(rows,columns);
+Print2DArray(arr1);
+Console.WriteLine();
+int[,] arr2 = Gen2DArray(rows,columns);
+Print2DArray(arr2);
+Console.WriteLine();
+int[,] resultMatrix = MatrixMultiplication(arr1,arr2);
+Print2DArray(resultMatrix);
---
+++ Seminar8Task60/Program.cs
@@ -0,0 +1,48 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+//Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Метод генерирует двумерный массив
+int[,,] Gen3DArray(int dim1, int dim2, int dim3)
+{
+    int[,,] arr = new int[dim1, dim2, dim3];
+    for (int i = 0; i < dim1; i++)
+    {
+        for (int j = 0; j < dim2; j++)
+        {
+            for (int k = 0; k < dim3; k++)
+            {
+                arr[i, j, k] = new Random().Next(1, 100);
+            }
+        }
+    }
+    return arr;
+}
+
+//Метод выводит двумерный массив на экран
+void Print3DArray(int[,,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                Console.Write(arr[i, j, k] + "(" + i + "," + j + "," + k +")" + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+int dim1= ReadData("Введите длину первого измерения: ");
+int dim2= ReadData("Введите длину второго измерения: ");
+int dim3= ReadData("Введите длину третьего измерения: ");
+int[,,] arr = Gen3DArray(dim1, dim2, dim3);
+Print3DArray(arr);
